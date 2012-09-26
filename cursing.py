543165aa1b48a80589caa037dc0b4e96d2ad59e4
sjzabel <sjzabel@gmail.com>
2012-09-26 09:10:22 -0500

    adding the runner

---
+++ cursing.py
@@ -0,0 +1,9 @@
+'''
+This is a launcher script for curses scripts
+
+eventually this will be the home for script options and loading default configuration
+'''
+from curses.wrapper import wrapper
+
+import logging
+
